dc7c26855486f6deec7ded023db9f3ecdf61f15c
Kenichi Fukushima <ken.fukushima@gmail.com>
2019-06-04 15:14:19 +0900

    simplification

--- main.tf
+++ main.tf
@@ -1,13 +1,16 @@
 module "users" {
-  source = "../modules/users"
+  source = "./modules/users"
 }
 
 module "profile" {
-  source = "../modules/profile"
+  source = "./modules/profile"
 
   users = module.users.defs
 
   profiles = {
+    "3" = {
+      location = "osaka"
+    }
     "4" = {
       location = "tokyo"
     }
